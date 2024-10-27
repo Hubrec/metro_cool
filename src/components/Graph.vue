26885581aaab14aa79feb92d48f32b7f055eb1d3
GUERIN MATHIS p2105770 <mathis.guerin@etu.univ-lyon1.fr>
2024-10-27 17:02:32 +0100

    backend connections and front update

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -16,16 +16,10 @@ export default {
 
     const container = document.getElementById('mynetwork');
 
-    const nodesData = await extractNodes()
-        .then((data) => {
-          return data;
-        })
+    const nodesData = await extractNodes();
     this.nodes = new DataSet(nodesData);
 
-    const linksData = await extractLinks()
-        .then((data) => {
-          return data;
-        })
+    const linksData = await extractLinks();
     this.edges = new DataSet(linksData);
 
     const data = {nodes: this.nodes, edges: this.edges};
@@ -33,7 +27,7 @@ export default {
     const basicOptions = {
       nodes: {
         shape: 'dot',
-        size: 16,
+        size: 4,
         color: {
           border: '#2B7CE9',
           background: '#97C2FC',
@@ -47,8 +41,8 @@ export default {
           }
         },
         font: {
-          size: 16,
-          color: '#dcdcdc'
+          size: 6,
+          color: 'rgba(0,0,0,0)'
         },
         borderWidth: 2,
         shadow: false
@@ -69,7 +63,7 @@ export default {
         shadow: false,
       },
       physics: {
-        enabled: true,
+        enabled: false,
       },
       interaction: {
         dragNodes: true,
@@ -78,102 +72,89 @@ export default {
         hover: true,
         multiselect: true,
       },
-      layout: {
-        randomSeed: 42,
-      }
     };
     // fancy settings
-    const options = {
-      nodes: {
-        shape: 'dot',
-        size: 16,
-        color: {
-          border: '#2B7CE9',
-          background: '#97C2FC',
-          highlight: {
-            border: '#2B7CE9',
-            background: '#D2E5FF'
-          },
-          hover: {
-            border: '#2B7CE9',
-            background: '#D2E5FF'
-          }
-        },
-        font: {
-          size: 16,
-          color: '#dcdcdc'
-        },
-        borderWidth: 2,
-        shadow: false
-      },
-      edges: {
-        width: 2,
-        color: {
-          color: '#848484',
-          highlight: '#848484',
-          hover: '#848484',
-          inherit: 'from',
-          opacity: 0.8
-        },
-        smooth: {
-          type: 'dynamic', // Change to 'dynamic' or 'straight' for better performance
-        },
-        shadow: false
-      },
-      physics: {
-        enabled: true, // Disable physics for better performance
-        barnesHut: {
-          gravitationalConstant: -8000,
-          centralGravity: 0.3,
-          springLength: 95,
-          springConstant: 0.04,
-          damping: 0.09,
-          avoidOverlap: 0.1
-        },
-        stabilization: {
-          enabled: true,
-          iterations: 1000,
-          updateInterval: 25,
-        }
-      },
-      interaction: {
-        dragNodes: true,
-        dragView: true,
-        zoomView: true,
-        hover: true,
-        multiselect: true,
-      },
-      layout: {
-        improvedLayout: true,
-        hierarchical: {
-          enabled: false,
-          levelSeparation: 150,
-          nodeSpacing: 100,
-          treeSpacing: 200,
-          blockShifting: true,
-          edgeMinimization: true,
-          parentCentralization: true,
-          direction: 'UD',
-          sortMethod: 'hubsize'
-        }
-      }
-    };
+    // const options = {
+    //   nodes: {
+    //     shape: 'dot',
+    //     size: 16,
+    //     color: {
+    //       border: '#2B7CE9',
+    //       background: '#97C2FC',
+    //       highlight: {
+    //         border: '#2B7CE9',
+    //         background: '#D2E5FF'
+    //       },
+    //       hover: {
+    //         border: '#2B7CE9',
+    //         background: '#D2E5FF'
+    //       }
+    //     },
+    //     font: {
+    //       size: 16,
+    //       color: '#dcdcdc'
+    //     },
+    //     borderWidth: 2,
+    //     shadow: false
+    //   },
+    //   edges: {
+    //     width: 2,
+    //     color: {
+    //       color: '#848484',
+    //       highlight: '#848484',
+    //       hover: '#848484',
+    //       inherit: 'from',
+    //       opacity: 0.8
+    //     },
+    //     smooth: {
+    //       type: 'dynamic', // Change to 'dynamic' or 'straight' for better performance
+    //     },
+    //     shadow: false
+    //   },
+    //   physics: {
+    //     enabled: true, // Disable physics for better performance
+    //     barnesHut: {
+    //       gravitationalConstant: -8000,
+    //       centralGravity: 0.3,
+    //       springLength: 95,
+    //       springConstant: 0.04,
+    //       damping: 0.09,
+    //       avoidOverlap: 0.1
+    //     },
+    //     stabilization: {
+    //       enabled: true,
+    //       iterations: 1000,
+    //       updateInterval: 25,
+    //     }
+    //   },
+    //   interaction: {
+    //     dragNodes: true,
+    //     dragView: true,
+    //     zoomView: true,
+    //     hover: true,
+    //     multiselect: true,
+    //   },
+    //   layout: {
+    //     improvedLayout: true,
+    //     hierarchical: {
+    //       enabled: false,
+    //       levelSeparation: 150,
+    //       nodeSpacing: 100,
+    //       treeSpacing: 200,
+    //       blockShifting: true,
+    //       edgeMinimization: true,
+    //       parentCentralization: true,
+    //       direction: 'UD',
+    //       sortMethod: 'hubsize'
+    //     }
+    //   }
+    // };
     this.network = new Network(container, data, basicOptions);
-    console.log(this.network)
   },
   methods: {
-    addNode() {
-      const newNode = { id: this.nodeIdCounter, label: `Node ${this.nodeIdCounter}`, x: Math.random() * 1000, y: Math.random() * 1000 };
-      this.nodes.add(newNode);
-      const randomNodeId = Math.floor(Math.random() * (this.nodes.get().length - 1)) + 1;
-      this.edges.add({ from: randomNodeId, to: this.nodeIdCounter });
-      this.nodeIdCounter++;
-    },
-
-    deleteSelectedNodes() {
-      this.network.getSelectedNodes().forEach((nodeId) => {
-        this.nodes.remove(nodeId);
-      });
+    printValues() {
+      console.log(this.nodes.get());
+      console.log(this.edges.get());
     }
   }
 }
@@ -182,19 +163,22 @@ export default {
 <template>
     <div id="mynetwork"></div>
     <div class="param-buttons">
-      <button class="buttonsActions" @click="addNode">Add node</button>
-      <button class="buttonsActions" @click="deleteSelectedNodes">Delete selected nodes</button>
+      <button class="buttonsActions" @click="printValues">Print Values</button>
     </div>
 </template>
 
 <style>
 #mynetwork {
   position: absolute;
-  top: 0;
-  left: 0;
-  height: 100vh;
+  top: 5vh;
+  left: 15vh;
+  height: 90vh;
+  width: 90vh;
   backdrop-filter: contrast(0.8);
   overflow: hidden;
+  background-image: url("../assets/metrof_r.png");
+  background-size: cover;
+  opacity: 0.95;
 }
 
 .param-buttons {
