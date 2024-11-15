6a5fcba904194d4bbe798bfa9ed8e65f3b1b47b7
GUERIN MATHIS p2105770 <mathis.guerin@etu.univ-lyon1.fr>
2024-11-15 17:47:14 +0100

    fix api local dev url

--- src/service/http/fetchAPI.js
+++ src/service/http/fetchAPI.js
@@ -3,12 +3,12 @@ const LOCALAPIURL = 'http://localhost:3000/api/data/';
 const APIURL = 'http://api.metrocool.mathisguerin.fr/api/data/';
 
 export async function extractNodes() {
-    const response = await fetch(APIURL + 'stations');
+    const response = await fetch(LOCALAPIURL + 'stations');
     return await response.json();
 }
 
 export async function extractLinks() {
-    const response = await fetch(APIURL + 'links');
+    const response = await fetch(LOCALAPIURL + 'links');
     return await response.json();
 }
 
